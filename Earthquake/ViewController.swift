1989b0ff6ac6f0472c446fb4d02c8fe54fbc54a6
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-10-05 13:55:27 +0900

    Initial Commit

---
+++ Earthquake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Earthquake
+//
+//  Created by D7703_17 on 2018. 10. 5..
+//  Copyright © 2018년 D7703_17. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+      override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+      }
+
+
+}
+
